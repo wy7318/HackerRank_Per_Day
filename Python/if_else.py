4bca56c8a6b55cf1a0abac5e20c66d27a92fd0a1
wy7318 <46336956+wy7318@users.noreply.github.com>
2022-04-13 17:01:26 -0700

    Update if_else.py

--- Python/if_else.py
+++ Python/if_else.py
@@ -1,10 +1,32 @@
-Task
+'''Task
 Given an integer, , perform the following conditional actions:
 
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
-Input Format
+If n is odd, print Weird
+If n is even and in the inclusive range 2 of 5 to , print Not Weird
+If n is even and in the inclusive range 6 of 20 to , print Weird
+If n is even and greater than 20, print Not Weird
 
-A single line containing a positive integer, .
+Input Format
+A single line containing a positive integer n.
+1<= n <= 100
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    if n%2 == 1:
+        print("Weird")
+    elif n%2 == 0 and 2<= n <= 5:
+        print("Not Weird")
+    elif n%2 == 0 and 6<= n <= 20:
+        print("Weird")
+    elif n%2 == 0 and 20 < n <= 100:
+        print("Not Weird")
+   
