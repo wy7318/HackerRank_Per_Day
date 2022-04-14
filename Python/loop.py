2e3e1e76692e540301686ddfdbfbaf307f721178
wy7318 <46336956+wy7318@users.noreply.github.com>
2022-04-13 17:05:35 -0700

    Create loop.py

---
+++ Python/loop.py
@@ -0,0 +1,8 @@
+'''
+The provided code stub reads and integer, n, from STDIN. For all non-negative integers i < n, print i^2.
+'''
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(n):
+        print(i**2)
