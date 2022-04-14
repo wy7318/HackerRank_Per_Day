9a278773f5392f94d805f70ac6b82955b3f37e0c
wy7318 <46336956+wy7318@users.noreply.github.com>
2022-04-13 17:08:37 -0700

    Create printFunction.py

---
+++ Python/printFunction.py
@@ -0,0 +1,21 @@
+'''
+The included code stub will read an integer, n, from STDIN.
+
+Without using any string methods, try to print the following:
+123...n
+Note that "..." represents the consecutive values in between.
+
+Example
+n=5
+Print the string 12345.
+'''
+
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    lst = []
+    for i in range(n):
+        lst.append(str(i+1))
+        
+    print(''.join(lst))
