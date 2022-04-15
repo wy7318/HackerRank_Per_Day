87f64c07dfe2bcbc8f7e7fd2fb5e65febcb188d7
wy7318 <46336956+wy7318@users.noreply.github.com>
2022-04-15 11:08:10 -0700

    Update if_else.py

--- Python/if_else.py
+++ Python/if_else.py
@@ -20,7 +20,7 @@ import sys
 
 
 if __name__ == '__main__':
-    n = int(raw_input().strip())
+    n = int(input().strip())
     if n%2 == 1:
         print("Weird")
     elif n%2 == 0 and 2<= n <= 5:
