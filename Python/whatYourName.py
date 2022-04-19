2c7c17d6a2752df715670545a1e254754d0eb076
wy7318 <46336956+wy7318@users.noreply.github.com>
2022-04-19 14:00:22 -0700

    Create whatYourName.py

---
+++ Python/whatYourName.py
@@ -0,0 +1,19 @@
+'''
+You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+ex)Hello firstname lastname! You just delved into python.
+'''
+
+
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    print("Hello " + first + " " + last + "! You just delved into python.")
+
+if __name__ == '__main__':
