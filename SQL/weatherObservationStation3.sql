d419b76d02db4991ccca2c40072e30cae3239278
wy7318 <46336956+wy7318@users.noreply.github.com>
2022-04-29 14:16:57 -0700

    Create weatherObservationStation3.sql

---
+++ SQL/weatherObservationStation3.sql
@@ -0,0 +1,5 @@
+/*
+Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+*/
+
+SELECT distinct(CITY) FROM STATION WHERE mod(ID, 2) = 0;
