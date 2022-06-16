a596f7a81612b763584e0a2393b2c14213a62cce
Matt Minsub Lee <46336956+wy7318@users.noreply.github.com>
2022-06-16 16:10:39 -0700

    Create weatherObservationStation15.sql

---
+++ SQL/weatherObservationStation15.sql
@@ -0,0 +1,5 @@
+/*
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4 decimal places
+*/
+
+SELECT CAST(LONG_W AS DECIMAL(10,4)) FROM STATION WHERE LAT_N = (SELECT max(LAT_N) FROM STATION WHERE LAT_N < 137.2345)
